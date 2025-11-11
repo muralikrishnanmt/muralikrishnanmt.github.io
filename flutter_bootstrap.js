5576190f5a63a00bac212c0a303d3cc230504f0c
Murali Krishnan MT <mtmuralikrishnan@gmail.com>
2025-11-11 12:52:48 +0530

    Added Experience Section in HomePage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1417625169"
+    serviceWorkerVersion: "2231725621"
   }
 });
